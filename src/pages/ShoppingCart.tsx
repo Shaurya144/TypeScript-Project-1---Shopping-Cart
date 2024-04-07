120996fe94b6a4c75da1e17a2f25030a1957b076
Shaurya Chundawat <chundawatshaurya@gmail.com>
2024-04-07 14:32:12 +0100

    Final Commit

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,3 +1,31 @@
-export function ShoppingCart(){
-  return <h1>Shopping</h1>
+import { Stack } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
+import { formatCurrency } from "../utilities/formatCurrency"
+import { CartItem } from "../components/CartItem"
+import storeItems from "../data/items.json"
+
+type ShoppingCartProps = {
+  isOpen: boolean
+}
+
+export function ShoppingCart({ isOpen }: ShoppingCartProps) {
+  const { closeCart, cartItems } = useShoppingCart()
+  return (  
+      <body>
+        <Stack  style={{padding:"50px"}} gap={3}>
+          {cartItems.map(item => (
+            <CartItem key={item.id} {...item} />
+          ))}
+          <div  style={{padding:"50px"}} className="ms-auto fw-bold fs-5">
+            Total{" "}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = storeItems.find(i => i.id === cartItem.id)
+                return total + (item?.price || 0) * cartItem.quantity
+              }, 0)
+            )}
+          </div>
+        </Stack>
+      </body>
+  )
 }
